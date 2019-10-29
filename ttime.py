cb98334d6fd21a934286953bec09b7d0d8bd5229
Alireza Niksejel <alireza.ns70@gmail.com>
2019-10-29 17:37:42 +0000

    Update ttime.py

--- ttime.py
+++ ttime.py
@@ -1,7 +1,3 @@
-'''
-Calculate P-phase arrivals.
-'''
-
 from pyrocko import cake
 import numpy as num
 km = 1000.
@@ -13,12 +9,11 @@ model = cake.load_model('/home/alireza/Desktop/Grid_Search/Velmodel.tvel')
 source_depth = 10 * km
 
 # Distances as a numpy array [deg].
-distances = num.linspace(295.97,295.97,1)*km * cake.m2d
+distances = num.linspace(295.97,295.97,1) * km * cake.m2d
 
 # Define the phase to use.
 Phase = cake.PhaseDef('P')
 
 # calculate distances and arrivals and print them:
-print 'distance [km]      time [s]'
 for arrival in model.arrivals(distances, phases=Phase, zstart=source_depth):
     print '%13g %13g' % (arrival.x*cake.d2m/km, arrival.t)
